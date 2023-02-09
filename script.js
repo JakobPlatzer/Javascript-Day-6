03bf544d8390dec929475217b6079ac10b17eacc
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-09 11:46:54 +0100

    first step

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Basic Exercise 
 
-let result = document.getElementById("result");
+// let result = document.getElementById("result");
 
-var sandwiches = { 
-    sandwich: "hamburger", 
-    calories: 260, };
-var fries = { 
-    fries_size: "Large French Fries", 
-    calories: 570, };
+// var sandwiches = { 
+//     sandwich": "hamburger", 
+//     calories: 260, };
+// var fries = { 
+//     fries_size": "Large French Fries", 
+//     calories: 570, };
 
-result.innerHTML = `My favorite sandwich is a ${sandwiches.sandwich} which has approximately ${sandwiches.calories} calories, along with it I enjoy eating ${fries.fries_size} which have about ${fries.calories} calories.`
+// result.innerHTML = `My favorite sandwich is a ${sandwiches.sandwich} which has approximately ${sandwiches.calories} calories, along with it I enjoy eating ${fries.fries_size} which have about ${fries.calories} calories.`
 
 // Intermediate Exercise 
+
+
+
+for(let val of employees) {
+    document.getElementById("employeeTable").innerHTML +=`
+            <tr>
+              <td scope="row">${val.id}</td>
+              <td>${val.firstName}</td>
+              <td>${val.lastName}</td>
+              <td>${val.email}</td>
+              <td>${val.jobTitle}</td>
+              <td>${val.Salary}</td>
+            </tr>
+`;
+}
